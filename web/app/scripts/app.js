0d71cee529cf9c0d17af740084948b2802e52108
Valentin Hamon <vhamon@teclib.com>
2016-09-30 09:11:56 +0200

    Refactor main module code

--- web/app/scripts/app.js
+++ web/app/scripts/app.js
@@ -51,52 +51,48 @@ var a6oApp = angular
   ])
   .config(function ($stateProvider, $urlRouterProvider, toastrConfig, $translateProvider) {
 	  
-	   angular.extend(toastrConfig, {
-			progressBar: false,
-			tapToDismiss: true,
-			timeOut: 5000,
-			maxOpened: 1
-		  });
+      angular.extend(toastrConfig, {
+		    progressBar: false,
+		    tapToDismiss: true,
+		    timeOut: 5000,
+		    maxOpened: 1
+	  });
      
-      //Enable a more secure variant escape 
       $translateProvider.useSanitizeValueStrategy('escape');
 
       $translateProvider.useStaticFilesLoader({
         prefix: 'scripts/filters/languages/',
         suffix: '.js'
       });
-      // Tell the module what language to use by default
+
       $translateProvider.preferredLanguage('en_US');
 
-   //
-  // For any unmatched url, redirect to /main
-  $urlRouterProvider.otherwise("/Main");
-  //
-  // Set up the states
-  $stateProvider
-    .state('Main', {
-      url: '/Main',
-      templateUrl: 'views/Main.html',
-      controller: 'MainController'
-    })
-    .state('Main.Information', {
-      url: '/Information',
-      templateUrl: 'views/Information.html',
-      controller: 'InformationController'
-    })
-    .state('Main.Scan', {
-      url: '/Scan',
-      templateUrl: 'views/Scan.html',
-      controller: 'ScanController'
-    })
-    .state('Main.Journal', {
-      url: '/Journal',
-      templateUrl: 'views/Journal.html',
-      controller: 'JournalController'
-    })
-    .state('Main.Parameters', {
-      url: '/Parameters',
-      templateUrl: 'views/Parameters.html',
-      controller: 'ParametersController'
-    });
+      $urlRouterProvider.otherwise("/Main");
+
+      $stateProvider
+        .state('Main', {
+          url: '/Main',
+          templateUrl: 'views/Main.html',
+          controller: 'MainController'
+        })
+        .state('Main.Information', {
+          url: '/Information',
+          templateUrl: 'views/Information.html',
+          controller: 'InformationController'
+        })
+        .state('Main.Scan', {
+          url: '/Scan',
+          templateUrl: 'views/Scan.html',
+          controller: 'ScanController'
+        })
+        .state('Main.Journal', {
+          url: '/Journal',
+          templateUrl: 'views/Journal.html',
+          controller: 'JournalController'
+        })
+        .state('Main.Parameters', {
+          url: '/Parameters',
+          templateUrl: 'views/Parameters.html',
+          controller: 'ParametersController'
+        });
 });
